1d4658f88bfedaff43299e7fd60ed181ee550432
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-04-18 17:21:29 +0200

    Overlay für Sehenswürdigkeiten definiert

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
+let themaLayer = {
+  sights: L.featureGroup().addTo(map)
+}
 // Hintergrundlayer
 L.control
   .layers({
@@ -25,6 +28,8 @@ L.control
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
     "OpenRailwayMap Zugnetz": L.tileLayer.provider("OpenRailwayMap"),
+  }, {
+    "Sehenswürdigkeiten": themaLayer.sights,
   })
   .addTo(map);
 
@@ -50,6 +55,6 @@ async function loadSights(url) {
   let response = await fetch(url);
   let geojson = await response.json();
   console.log(geojson);
-  L.geoJSON(geojson).addTo(map);
+  L.geoJSON(geojson).addTo(themaLayer.sights);
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
